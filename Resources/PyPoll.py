6966a109d81a9e6fb877d81a94bba1f9e96a9d8e
pat-tsai <67960210+pat-tsai@users.noreply.github.com>
2020-07-31 22:27:45 -0700

    election analysis

---
+++ Resources/PyPoll.py
@@ -0,0 +1,13 @@
+import datetime as dt
+# modules --> classes --> attributes
+
+
+# data needed to retreieve
+
+# 1. Total number of votes cast
+# 2. Complete list of candidates who received votes
+# 3. Percentage of votes each candidate received
+# 4. Total number of votes each candidate received
+# 5. Winner of election based upon popular vote
+
+print(dir(int))
